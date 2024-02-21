e581bbf19eb54a442f3b91dce258f4b714bd8bb3
Chau MAk Ka Ra <rab2014779@student.ctu.edu.vn>
2024-02-21 22:13:50 +0700

    Trang thông báo

--- src/adminpage/Notifications/index.js
+++ src/adminpage/Notifications/index.js
@@ -1,49 +1,28 @@
 import clsx from 'clsx';
 import styles from './Notifications.module.scss';
 import { useState } from 'react';
-import axios from 'axios';
 
 function Notifications() {
     const [notification, setNotification] = useState('');
-    const [isPrivate, setIsPrivate] = useState(false);
 
-    const sendNotification = async () => {
-        try {
-            const notificationType = isPrivate ? 'private' : 'public';
-            await axios.post('/api/sendNotification', { notification, notificationType });
-            alert('Notification sent successfully');
-        } catch (error) {
-            console.error('Error sending notification', error);
-            alert('Failed to send notification');
-        }
-    };
+    const sendNotification = async () => {};
     return (
         <div className={clsx(styles.wrapper)}>
             <div className="container mt-5">
-                <h2>Admin Notification Page</h2>
-                <div className="form-group">
-                    <textarea
-                        className="form-control"
-                        value={notification}
-                        onChange={(e) => setNotification(e.target.value)}
-                        placeholder="Enter notification message"
-                    />
+                <h2>Quản lý gửi thông báo</h2>
+                <div className={clsx(styles.form)}>
+                    <div className={clsx(styles.form_group, 'form-group')}>
+                        <textarea
+                            className={clsx(styles.form_control, 'form-control')}
+                            value={notification}
+                            onChange={(e) => setNotification(e.target.value)}
+                            placeholder="Enter notification message"
+                        />
+                    </div>
+                    <button onClick={sendNotification} className={clsx(styles.btn, 'btn btn-primary')}>
+                        Gửi thông báo
+                    </button>
                 </div>
-                <div className="form-check mb-3">
-                    <input
-                        type="checkbox"
-                        className="form-check-input"
-                        id="privateCheck"
-                        checked={isPrivate}
-                        onChange={() => setIsPrivate(!isPrivate)}
-                    />
-                    <label className="form-check-label" htmlFor="privateCheck">
-                        Private Notification
-                    </label>
-                </div>
-                <button onClick={sendNotification} className="btn btn-primary">
-                    Send Notification
-                </button>
             </div>
         </div>
     );
