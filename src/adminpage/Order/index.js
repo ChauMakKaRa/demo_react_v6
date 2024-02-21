ce5ebaf4a57ecb0d045678adbb7fffa78166e62f
Chau MAk Ka Ra <rab2014779@student.ctu.edu.vn>
2024-02-21 22:14:10 +0700

    Trang đơn hàng của admin

--- src/adminpage/Order/index.js
+++ src/adminpage/Order/index.js
@@ -1,7 +1,7 @@
 import clsx from 'clsx';
 import styles from './Order.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faPrint, faSearch } from '@fortawesome/free-solid-svg-icons';
 import Button from '@/components/Button';
 import TableOrder from './TableOrder';
 import { useEffect, useState } from 'react';
@@ -10,18 +10,32 @@ import api from '@/config-api';
 function OrderPageAdmin() {
     const [orders, setOrders] = useState([]);
 
+    const [selectedValue, setSelectedValue] = useState('filter');
+    const [tempValue, setTempValue] = useState('filter');
+
+    const handleDropdownChange = (event) => {
+        setSelectedValue(event.target.value);
+    };
+
+    const fetchData = async (selectedValue) => {
+        try {
+            const respones = await axios.get(`${api.get_all_order}?value=${selectedValue}`);
+            const data = respones.data;
+            setOrders(data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const handleSubmitOption = () => {
+        console.log(selectedValue);
+        fetchData(selectedValue);
+        setTempValue(tempValue);
+    };
+
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const respones = await axios.get(`${api.get_all_order}`);
-                const data = respones.data;
-                setOrders(data);
-            } catch (error) {
-                console.log(error);
-            }
-        };
-        fetchData();
-    }, []);
+        fetchData(tempValue);
+    }, [tempValue]);
     return (
         <div className={clsx(styles.wrapper)}>
             <div className={clsx(styles.page_manager_order)}>
@@ -30,21 +44,25 @@ function OrderPageAdmin() {
                     <div className={clsx(styles.delete_div)}>
                         <Button className={clsx(styles.btn_delete)}>Xóa đơn hàng</Button>
                     </div>
+                    <div className={clsx(styles.print_biil)}>
+                        <Button className={clsx(styles.btn_print_biil)}>In đơn</Button>
+                        <FontAwesomeIcon icon={faPrint} />
+                    </div>
                     <div className={clsx(styles.no_content)}></div>
 
-                    <div className={clsx(styles.select_option)}>
+                    <div className={clsx(styles.select_option)} value={selectedValue} onChange={handleDropdownChange}>
                         <select id={clsx(styles.my_select)}>
-                            <option value="filter">Tất cả</option>
-                            <option value="Chưa duyệt">Chưa duyệt</option>
-                            <option value="Đã duyệt">Đã duyệt</option>
-                            <option value="Đã thanh toán">Đã thanh toán</option>
+                            <option value="all">Tất cả</option>
+                            <option value="Chờ duyệt">Chưa duyệt</option>
+                            <option value="Đang giao">Đã duyệt</option>
+                            <option value="Đã giao">Đã thanh toán</option>
                         </select>
                     </div>
                     <div className={clsx(styles.btn_submit)}>
-                        <Button>Áp dụng</Button>
+                        <Button onClick={handleSubmitOption}>Áp dụng</Button>
                     </div>
                     <div className={clsx(styles.search)}>
-                        <input type="text" placeholder="Nhập số điện thoại..." />
+                        <input type="text" placeholder="Nhập id khách hàng..." />
                         <FontAwesomeIcon className={clsx(styles.icon_search)} icon={faSearch} />
                     </div>
                 </div>
