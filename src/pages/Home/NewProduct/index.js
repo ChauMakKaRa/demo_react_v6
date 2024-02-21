10583de5cb03f30413aa61673dbec18747111a6b
Chau MAk Ka Ra <rab2014779@student.ctu.edu.vn>
2024-02-21 22:17:22 +0700

    trang người dùng

--- src/pages/Home/NewProduct/index.js
+++ src/pages/Home/NewProduct/index.js
@@ -1,11 +1,32 @@
 import clsx from 'clsx';
 import styles from './NewProduct.module.scss';
 import Button from '@/components/Button';
+import { useEffect, useState } from 'react';
+import ReactPaginate from 'react-paginate';
 
 function NewProduct({ products }) {
+    const [pageCounts, setPageCounts] = useState(0);
+    const [currentPage, setCurrentPage] = useState(0);
+
+    const productsPerPage = 10;
+    useEffect(() => {
+        try {
+            if (products) {
+                setPageCounts(Math.ceil(products.length / productsPerPage));
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }, [products]);
+    const handlePageClick = ({ selected }) => {
+        setCurrentPage(selected);
+    };
+    const offset = currentPage * productsPerPage;
+    const currentProducts = products.slice(offset, offset + productsPerPage);
+    const reversedProducts = currentProducts.slice().reverse();
     return (
         <div className={clsx(styles.wrapper)}>
-            {products.map((product) => (
+            {reversedProducts.map((product) => (
                 <Button to={`/detail-product?id=${product.id}`} key={product.id}>
                     <div className={clsx(styles.product_list)}>
                         <div className={clsx(styles.item)}>
@@ -21,6 +42,27 @@ function NewProduct({ products }) {
                     </div>
                 </Button>
             ))}
+            <div className={clsx(styles.react_paginate, 'custom-pagination-class')}>
+                <ReactPaginate
+                    breakLabel="..."
+                    nextLabel="Sau"
+                    onPageChange={handlePageClick}
+                    pageRangeDisplayed={5}
+                    pageCount={pageCounts}
+                    previousLabel="Trước"
+                    renderOnZeroPageCount={null}
+                    pageClassName="page-item"
+                    pageLinkClassName="page-link"
+                    previousClassName="page-item"
+                    previousLinkClassName="page-link"
+                    nextClassName="page-item"
+                    nextLinkClassName="page-link"
+                    breakClassName="page-item"
+                    breakLinkClassName="page-link"
+                    containerClassName="pagination"
+                    activeClassName="active"
+                />
+            </div>
         </div>
     );
 }
